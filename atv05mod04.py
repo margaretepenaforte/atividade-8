045c0792d66ddb49d9bde520adb3a43b076047c5
MARGARETE PENAFORTE <109976418+margaretepenaforte@users.noreply.github.com>
2022-08-16 08:53:12 -0300

    Add files via upload
    manipulação de caracteres em maiúsculas e minúsculas - strings

---
+++ atv05mod04.py
@@ -0,0 +1,15 @@
+#manipulando caracteres em maiusculas e minusculas - string
+
+name = "margarete penaforte"
+
+print( name.title() )
+print( name.upper() )
+print( name.lower() )
+
+first_name = "margarete"
+last_name = "penaforte"
+full_name = first_name + " " + last_name
+
+print( full_name )
+
+print( "Olá, " + full_name.title() + "!" )
